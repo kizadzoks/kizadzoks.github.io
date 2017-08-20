afd17d983a8cccb3ceddf87131eb4d5d27b49094
pilgrim011 <printerstampac@gmail.com>
2017-08-20 23:17:23 +0200

    404 modify and php testing

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="/main.css">
+    <title>Test page</title>
+  </head>
+  <body>
+<h1>Test page</h1>
+  </body>
+</html>
